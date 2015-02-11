5b241dfc368dcfdbfb510996a84f90fdaeda2d4f
Mikhail Savkin <savkinmk@gmail.com>
2015-02-11 11:53:24 +0300

    Add launcher for StrongNameRemover

---
+++ ThirdParty/StrongNameRemover/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using Reflexil.Forms;
+
+namespace StrongNameRemover
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new StrongNameRemoverForm {FormBorderStyle = FormBorderStyle.Sizable, ShowInTaskbar = true});
+		}
+	}
+}
